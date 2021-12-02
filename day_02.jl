479652a0e7ddb49d74f740cbc7e5292e130eafe5
Tom Gillam <thomas.gillam@invenialabs.co.uk>
2021-12-02 09:31:35 +0000

    Complete day 2

--- day_02.jl
+++ day_02.jl
@@ -21,8 +21,7 @@ function parse_line(line::AbstractString)
     return (Symbol(direction), parse(Int64, amount))
 end
 
-function get_final_position(commands::AbstractString)
-    position = Position()
+function get_final_position(commands::AbstractString, position=Position())
     for line in split(commands, "\n")
         isempty(line) && continue
         direction, amount = parse_line(line)
@@ -33,7 +32,9 @@ end
 
 """Multiply two components to get final answer."""
 get_ans(position::Position) = position.horizontal * position.depth
-get_ans(commands::AbstractString) = get_ans(get_final_position(commands))
+function get_ans(commands::AbstractString, initial=Position())
+    return get_ans(get_final_position(commands, initial))
+end
 
 example = """
 forward 5
@@ -55,3 +56,27 @@ commands = _load_input("day_02.txt")
 get_ans(commands)
 
 # Part 2
+
+mutable struct PositionAim
+    horizontal::Int64
+    depth::Int64
+    aim::Int64
+end
+PositionAim() = PositionAim(0, 0, 0)
+
+get_ans(position::PositionAim) = position.horizontal * position.depth
+
+function move!(state::PositionAim, direction::Symbol, amount::Integer)
+    if direction == :forward
+        state.horizontal += amount
+        state.depth += state.aim * amount
+    elseif direction == :down
+        state.aim += amount
+    elseif direction == :up
+        state.aim -= amount
+    end
+end
+
+@assert get_ans(example, PositionAim()) == 900
+
+get_ans(commands, PositionAim())
