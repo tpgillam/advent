21a0c0e5f27ffad9641b627155b0f40a551ad8ea
Tom Gillam <thomas.gillam@invenialabs.co.uk>
2021-12-04 21:00:59 +0000

    Complete day 4

--- day_04.jl
+++ day_04.jl
@@ -64,13 +64,21 @@ function Base.parse(::Type{Bingo}, input::AbstractString)
     return Bingo(order, size_, boards)
 end
 
-function get_winning_score(bingo::Bingo)
+"""
+Get the winning (losing) score if `winning` is true (false).
+"""
+function get_score(bingo::Bingo, winning::Bool)
     markers = [zeros(bingo.size) for _ in 1:length(bingo.boards)]
     row_markers = [zeros(bingo.size[1]) for _ in 1:length(bingo.boards)]
     col_markers = [zeros(bingo.size[2]) for _ in 1:length(bingo.boards)]
 
+    completed_boards = Set{Int64}()
+
     for number in bingo.order
         @inbounds for i_board in 1:length(bingo.boards)
+            # If this board is done, skip it :)
+            i_board in completed_boards && continue
+
             board = bingo.boards[i_board]
             marker = markers[i_board]
             row_marker = row_markers[i_board]
@@ -90,15 +98,29 @@ function get_winning_score(bingo::Bingo)
             )
             complete || continue
 
-            # We have finished! compute the score and return it.
+            # We have finished!
+            push!(completed_boards, i_board)
+
+            # If we're looking for the losing board, only return if all are complete.
+
+            !winning && (length(completed_boards) < length(bingo.boards)) && continue
+
+            # Compute and return the score.
             unmarked_sum = sum(board .* (marker .< 1))
             return unmarked_sum * number
         end
     end
 end
 
+get_winning_score(bingo::Bingo) = get_score(bingo, true)
+get_losing_score(bingo::Bingo) = get_score(bingo, false)
+
 
 bingo = parse(Bingo, example)
 @assert get_winning_score(bingo) == 4512
 
 get_winning_score(parse(Bingo, load_input(4)))
+
+# Part 2
+@assert get_losing_score(parse(Bingo, example)) == 1924
+get_losing_score(parse(Bingo, load_input(4)))
