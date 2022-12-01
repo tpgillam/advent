1dfd188113a0b10946e3953015dcc2c8747709c4
Tom Gillam <tpgillam@googlemail.com>
2022-12-01 16:45:11 +0000

    Done day 1

--- 2022/common.jl
+++ 2022/common.jl
@@ -3,6 +3,6 @@ using Printf
 function load_input(day::Integer)
     day = @sprintf("%0.2d", day)
     name = "day_$(day).txt"
-    path = joinpath("inputs", name)
+    path = joinpath("2022", "inputs", name)
     return read(path, String)
 end
--- 2022/day_01.jl
+++ 2022/day_01.jl
@@ -1,6 +1,49 @@
 include("common.jl")
 
+struct Item
+    calories::Int
+end
 
+struct Elf
+    items::Vector{Item}
+end
+
+calories(item::Item) = item.calories
+calories(elf::Elf) = sum(calories, elf.items)
+calories(elves::Vector{Elf}) = sum(calories, elves)
+
+Base.parse(::Type{Item}, str::AbstractString) = Item(parse(Int, str))
+
+
+"""Flush `seen_items` into `elves`."""
+function _flush!(elves, seen_items)
+    isempty(seen_items) && return seen_items
+    push!(elves, Elf(seen_items))
+    return Item[]
+end
+
+function Base.parse(::Type{Vector{Elf}}, input::AbstractString)
+    elves = Elf[]
+    seen_items = Item[]
+
+    for line in split(input, '\n')
+        if isempty(line)
+            seen_items = _flush!(elves, seen_items)
+            continue
+        end
+        item = parse(Item, line)
+        push!(seen_items, item)
+
+    end
+    seen_items = _flush!(elves, seen_items)
+    return elves
+end
+
+
+function get_ans(input::AbstractString)
+    elves = parse(Vector{Elf}, input)
+    return maximum(calories, elves)
+end
 
 example = """
 1000
@@ -18,3 +61,18 @@ example = """
 
 10000
 """
+
+@assert get_ans(example) == 24000
+get_ans(load_input(1))
+
+# Part 2
+
+function get_ans2(input::AbstractString)
+    elves = parse(Vector{Elf}, input)
+    all_calories = map(calories, elves)
+    partialsort!(all_calories, 3; rev=true)
+    return sum(all_calories[1:3])
+end
+
+@assert get_ans2(example) == 45000
+get_ans2(load_input(1))
