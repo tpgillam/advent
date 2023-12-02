28e07d19bf6231444f12529df31fe2702402d28d
Tom Gillam <tpgillam@googlemail.com>
2023-12-02 01:17:49 +0000

    Finally...

--- 2023/day-01/src/main.rs
+++ 2023/day-01/src/main.rs
@@ -28,56 +28,77 @@ fn part1(input: &str) -> String {
 }
 
 // Replace any occurrences of known digits with digits.
-// NOTE: We must always replace the first valid string seen first!
-//  so "eightwo" -> "8wo" (and not "eigh2")
+// NOTE: We always replace ALL digits, even overlapping. To rationalise this,
+//  we only replace the first character of the number with the digit, e.g.
+//      "eightwo" -> "8igh2wo" 
+//  This means that the resulting string has the same length as the input.
 fn replace_string_numbers(line: &str) -> String {
+    // Find all the literal numbers in the string.
+    let digit_to_locations = find_literal_digit_occurrences(line);
+
+    // NOTE: For future learning...
+    // Initially I came up with the following contorted code:
+    //
+    //      let line_new: Vec<u8> = line.as_bytes().into_iter().map(|&x| x).collect();
+    //
+    // What's happening here is that `as_bytes` is referring to (immutable) bytes
+    // stored inside `line`. So the weird `map` call is required to actually do a copy.
+    //
+    // The below is better -- here we do the copy in one go with `to_string`, and then
+    // use `into_bytes` to consume the String and get a Vec<u8> (which is owning).
+    let mut line_bytes = line.to_string().into_bytes();
+
+    for (&digit, locations) in digit_to_locations.iter() {
+        for &loc in locations.iter() {
+            line_bytes[loc] = digit;
+        }
+    }
+
+    return String::from_utf8(line_bytes).unwrap();
+}
+
+// Return a map from digit (as an ASCII byte) to the locations in `str` at which it occurs.
+fn find_literal_digit_occurrences(line: &str) -> HashMap<u8, Vec<usize>> {
     let numbers = vec![
         "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
     ];
 
-    let mut line_new = line.to_string();
+    let result: HashMap<_, _> = numbers
+        .iter()
+        .enumerate()
+        .filter_map(|(i, &number)| {
+            let matches: Vec<_> = line.match_indices(number).collect();
 
-    loop {
-        // Look for replacements that we can make in the line, and store
-        // the index where the substring starts.
-        let i_number_to_loc: HashMap<_, _> = numbers
-            .iter()
-            .enumerate()
-            .filter_map(|(i, number)| {
-                let loc = line_new.find(number)?;
-                Some((i, loc))
-            })
-            .collect();
-
-        if i_number_to_loc.is_empty() {
-            // There are no replacements left to make.
-            break;
-        }
-
-        // Perform only the first replacement that can be made; we then repeat the process
-        // above.
-        let (&i_number, _) = i_number_to_loc.iter().min_by_key(|(_, &loc)| loc).unwrap();
-        line_new = line_new.replacen(
-            numbers.get(i_number).unwrap(),
-            &(i_number + 1).to_string(),
-            1,
-        );
-    }
-
-    return line_new;
+            if matches.is_empty() {
+                return None;
+            } else {
+                let digit_bytes = (i + 1).to_string().into_bytes();
+                assert_eq!(digit_bytes.len(), 1);
+                return Some((
+                    digit_bytes[0],
+                    matches.iter().map(|&(loc, _)| loc).collect::<Vec<_>>(),
+                ));
+            }
+        })
+        .collect();
+    result
 }
 
 fn part2(input: &str) -> String {
     let answer: u32 = input
         .lines()
         .map(|line| {
-            let digits: Vec<_> = replace_string_numbers(line).chars().filter_map(|x| {
-                if x.is_ascii_digit() {
-                    Some(x.to_string().parse::<u32>().unwrap())
-                } else {
-                    None
-                }
-            }).collect();
+
+            let digits: Vec<_> = replace_string_numbers(line)
+                .chars()
+                .filter_map(|x| {
+                    if x.is_ascii_digit() {
+                        Some(x.to_string().parse::<u32>().unwrap())
+                    } else {
+                        None
+                    }
+                })
+                .collect();
             let first_digit = digits.first().unwrap();
             let last_digit = digits.last().unwrap();
             first_digit * 10 + last_digit
