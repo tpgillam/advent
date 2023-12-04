ff30c41207f02f4a508d15dde4c46e7b4ba51409
Tom Gillam <tpgillam@googlemail.com>
2023-12-04 21:55:58 +0000

    Second example passes

--- 2023/day-04/src/main.rs
+++ 2023/day-04/src/main.rs
@@ -24,9 +24,9 @@ impl FromStr for Card {
 
     fn from_str(s: &str) -> Result<Card, ParseCardError> {
         let contents = match s.split_once(": ") {
-            Some((_, contents)) => contents,
-            None => return Err(ParseCardError),
-        };
+            Some((_, contents)) => Ok(contents),
+            None => Err(ParseCardError),
+        }?;
 
         let (winning_numbers_str, our_numbers_str) = match contents.split_once(" | ") {
             Some(x) => x,
@@ -42,39 +42,105 @@ impl FromStr for Card {
     }
 }
 
+fn num_winning(card: &Card) -> u32 {
+    let winning: HashSet<u32> = card.winning_numbers.iter().cloned().collect();
+    let ours: HashSet<u32> = card.our_numbers.iter().cloned().collect();
+    ours.intersection(&winning).count() as u32
+}
+
 fn part1(input: &str) -> String {
     let cards: Vec<Card> = input.trim().lines().map(|x| x.parse().unwrap()).collect();
-    let answer: u32 = cards.iter().map(|card| {
-        let winning: HashSet<u32> = card.winning_numbers.iter().cloned().collect();
-        let ours: HashSet<u32> = card.our_numbers.iter().cloned().collect();
-        let num_winning = ours.intersection(&winning).count() as u32;
-        match num_winning {
-            0 => 0,
-            _ => 2u32.pow(num_winning - 1) as u32
-        }
-    }).sum();
+    let answer: u32 = cards
+        .iter()
+        .map(|card| {
+            let n = num_winning(card);
+            match n {
+                0 => 0,
+                _ => 2u32.pow(n - 1) as u32,
+            }
+        })
+        .sum();
+    answer.to_string()
+}
+
+struct Replicator {
+    /// The number of copies that should be made.
+    num_copies: u32,
+
+    /// The number of remaining cards to which this applies.
+    remaining_lifetime: u32,
+}
+
+fn part2(input: &str) -> String {
+    // We keep track of a stack of replicators; each entry has a 'time-to-live',
+    // which is decremented as we go through the pack. It also indicates how many
+    // copies should be made.
+    let mut replicators: Vec<Replicator> = Vec::new();
+
+    let cards = input.trim().lines().map(|x| x.parse().unwrap());
+    let answer: u32 = cards
+        .map(|card| {
+            // First we score this card; i.e. figure out how many of it we have.
+            // One for the initial copy, and then we sum up the number of copies to make
+            let num_copies = 1 + replicators.iter().map(|x| x.num_copies).sum::<u32>();
+
+            // Update the existing replicators...
+            replicators = replicators
+                .iter()
+                .filter_map(|x| {
+                    match x.remaining_lifetime {
+                        0 => unreachable!(), // We should never create a replicator with a lifetime of 0.
+                        1 => None,
+                        n => Some(Replicator {
+                            num_copies: x.num_copies,
+                            remaining_lifetime: n - 1,
+                        }),
+                    }
+                })
+                .collect();
+
+            // ... and add a new replicator if required.
+            match num_winning(&card) {
+                0 => {}
+                n => replicators.push(Replicator {
+                    num_copies,
+                    remaining_lifetime: n,
+                }),
+            }
+
+            // We return the number of copies; we will aggregate these.
+            num_copies
+        })
+        .sum();
+
     answer.to_string()
 }
 
 fn main() {
     let input = get_input();
     println!("Part1: {}", part1(input));
-    // println!("Part2: {}", part2(input));
+    println!("Part2: {}", part2(input));
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::part1;
+    use crate::{part1, part2};
 
-    #[test]
-    fn part1_example() {
-        let example = "
+    const EXAMPLE: &str = "
 Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
 Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
-        assert_eq!(part1(example), "13")
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(part1(EXAMPLE), "13")
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(EXAMPLE), "30")
     }
 }
