1be110a752858aefce0b2b4ecbf0e38811cf1433
Tom Gillam <tpgillam@googlemail.com>
2025-12-15 00:43:32 +0000

    done day 3

--- 2025/day_03.zig
+++ 2025/day_03.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 // Maximum length of any line; we uw
 const MAX_LINE_LENGTH = 512;
 
-fn maxJoltage(line: []const u8) u32 {
+fn maxJoltage2(line: []const u8) u32 {
     // The 'tens' place is the largest digit found in [0:len(line) - 2].
     // The 'ones' place is the largest digit found after the 'tens' place.
     //
@@ -31,12 +31,60 @@ fn maxJoltage(line: []const u8) u32 {
     return 10 * tens + ones;
 }
 
+fn maxJoltageN(line: []const u8, comptime n: usize) u64 {
+    var digits: [n]u64 = .{0} ** n;
+
+    outer: for (line, 0..) |x, i| {
+        const val = x - '0';
+        for (0..n) |i_digit| {
+            // Compute the exponent of 10 that gives us the multiplier of this
+            // digit.
+            const exponent = (n - i_digit) - 1;
+            if (i < (line.len - exponent)) {
+                if (val > digits[i_digit]) {
+                    // Set this exponent; this means we must invalidate the
+                    // remaining digits, and the continue.
+                    digits[i_digit] = val;
+
+                    // NOTE: to perform the invalidation we need only
+                    // invalidate the _next_ digit, if there is one.
+                    if ((i_digit + 1) < n) digits[i_digit + 1] = 0;
+
+                    // Abort the inner loop over exponents, and move onto the
+                    // next character.
+                    continue :outer;
+                }
+            }
+
+            // std.debug.print("{d}\n", .{i_digit});
+            // std.debug.print("{any}\n", .{digits});
+        }
+    }
+
+    var total: u64 = 0;
+    var multiplier: u64 = 1;
+    for (0..n) |i| {
+        total += multiplier * digits[n - 1 - i];
+        multiplier *= 10;
+    }
+    return total;
+}
+
 fn part1(reader: *std.io.Reader) !u32 {
     var total: u32 = 0;
     while (try reader.takeDelimiter('\n')) |line| {
         if (line.len == 0) break;
         // std.debug.print("{s}\n", .{line});
-        total += maxJoltage(line);
+        total += maxJoltage2(line);
+    }
+    return total;
+}
+
+fn part2(reader: *std.io.Reader) !u64 {
+    var total: u64 = 0;
+    while (try reader.takeDelimiter('\n')) |line| {
+        if (line.len == 0) break;
+        total += maxJoltageN(line, 12);
     }
     return total;
 }
@@ -49,16 +97,20 @@ pub fn main() !void {
     var reader_wrapper = file.reader(&buffer);
     std.debug.print("Part 1: {d}\n", .{try part1(&reader_wrapper.interface)});
     try reader_wrapper.seekTo(0);
-    std.debug.print("Part 1: {d}\n", .{try part1(&reader_wrapper.interface)});
+    std.debug.print("Part 2: {d}\n", .{try part2(&reader_wrapper.interface)});
 }
 
+const example_input =
+    \\987654321111111
+    \\811111111111119
+    \\234234234234278
+    \\818181911112111
+;
 test "example1" {
-    const example_input =
-        \\987654321111111
-        \\811111111111119
-        \\234234234234278
-        \\818181911112111
-    ;
     var reader = std.io.Reader.fixed(example_input);
     try std.testing.expect(try part1(&reader) == 357);
+}
+test "example2" {
+    var reader = std.io.Reader.fixed(example_input);
+    try std.testing.expect(try part2(&reader) == 3121910778619);
 }
