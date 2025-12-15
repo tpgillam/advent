ce9a447f7601821cf57546637f99f6faa7798d97
Tom Gillam <tpgillam@googlemail.com>
2025-12-15 00:09:49 +0000

    part 1 done

--- 2025/day_03.zig
+++ 2025/day_03.zig
@@ -3,12 +3,40 @@ const std = @import("std");
 // Maximum length of any line; we uw
 const MAX_LINE_LENGTH = 512;
 
-fn part1(reader: *std.io.Reader) !i32 {
-    var total: i32 = 0;
+fn maxJoltage(line: []const u8) !u32 {
+    // The 'tens' place is the largest digit found in [0:len(line) - 2].
+    // The 'ones' place is the largest digit found after the 'tens' place.
+    //
+    // Initialise to zero; this is a useful placeholder as we know that we
+    // will not read a zero.
+    var tens: u32 = 0;
+    var ones: u32 = 0;
+
+    for (line, 0..) |x, i| {
+        const val = try std.fmt.charToDigit(x, 10);
+        if (i < (line.len - 1)) {
+            // We can look for a tens.
+            if (val > tens) {
+                // We want to update our tens place; this means we have
+                // invalidated our previous choice for `ones`.
+                tens = val;
+                ones = 0;
+                continue;
+            }
+        }
+
+        if (val > ones) ones = val;
+    }
+
+    return 10 * tens + ones;
+}
+
+fn part1(reader: *std.io.Reader) !u32 {
+    var total: u32 = 0;
     while (try reader.takeDelimiter('\n')) |line| {
         if (line.len == 0) break;
-        std.debug.print("{s}\n", .{line});
-        total += 1;
+        // std.debug.print("{s}\n", .{line});
+        total += try maxJoltage(line);
     }
     return total;
 }
@@ -22,7 +50,6 @@ pub fn main() !void {
     std.debug.print("Part 1: {d}\n", .{try part1(&reader_wrapper.interface)});
     try reader_wrapper.seekTo(0);
     std.debug.print("Part 1: {d}\n", .{try part1(&reader_wrapper.interface)});
-
 }
 
 test "example1" {
